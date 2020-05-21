ce6ba957d00995a076612fbf6aa13dcde45e4dd3
Julie Park <jspark721@gmail.com>
2020-05-21 15:36:23 -0700

    initial commit

---
+++ js/index.js
@@ -0,0 +1,69 @@
+// ANIMATION
+const header = document.querySelector('#header');
+const banner = document.querySelector('#bannerBg');
+const bannerAddition = document.querySelector('#bannerBgAddition');
+const h1Line = document.querySelectorAll('.h-line span');
+const contentP = document.querySelector('.content-inner p');
+const mainButton = document.querySelector('.content-inner a');
+const bannerTimeline = gsap.timeline();
+
+bannerTimeline
+  .from([bannerAddition, banner], {
+    duration: 2,
+    width: 0,
+    skewX: 4,
+    ease: 'power3.inOut',
+    stagger: {
+      amount: 0.2,
+    },
+  })
+  .to(banner, {
+    width: '50%',
+    ease: 'power3.inOut',
+    duration: 1.5,
+  })
+  .from(header, {
+    delay: -0.5,
+    y: 16,
+    opacity: 0,
+    duration: 0.8,
+    ease: 'power3.inOut',
+  })
+  .from(h1Line, {
+    delay: -0.4,
+    y: 80,
+    duration: 0.8,
+    ease: 'power3.out',
+    stagger: {
+      amount: 0.2,
+    },
+  })
+  .from([contentP, mainButton], {
+    delay: -0.6,
+    y: -40,
+    duration: 0.8,
+    opacity: 0,
+    ease: 'power3.out',
+    stagger: {
+      amount: 0.2,
+    },
+  });
+
+//SCROLLMAGIC
+
+// const controller = new ScrollMagic.Controller();
+// const scene = new ScrollMagic.Scene({
+//   triggerElement: '.line',
+//   triggerHook: 0,
+//   reverse: false,
+// })
+//   // .addIndicators()
+//   .setTween(featuresTimeline)
+//   .addTo(controller);
+
+// MENU NAVIGATION
+const menuIcon = document.querySelector('.menu-icon');
+const menu = document.getElementById('menu');
+
+menu.style.display = 'none';
+console.log(menu);
